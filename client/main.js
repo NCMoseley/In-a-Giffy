e6f0fd5a2d5d816487b486b45db5853f9fcce328
Late Worm <travis.rempel@gmail.com>
2018-02-08 22:17:00 -0800

    Set up Router.

--- client/main.js
+++ client/main.js
@@ -2,7 +2,19 @@ import { Meteor } from "meteor/meteor";
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import App from "../imports/ui/containers/App";
 import "./main.css";
 
-Meteor.startup(() => ReactDOM.render(<App />, document.getElementById("root")));
+const InAGiffy = () => (
+  <Router>
+    <Switch>
+      <Route path="/" component={App} />
+    </Switch>
+  </Router>
+);
+
+Meteor.startup(() =>
+  ReactDOM.render(<InAGiffy />, document.getElementById("root"))
+);
