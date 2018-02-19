4759f3aad3b844f47d8aaa1bde8a8b3249b245f9
NCMoseley <ncmoseley@gmail.com>
2018-02-19 01:14:13 -0800

    Change timeout time

--- imports/ui/components/SubmitPage/index.js
+++ imports/ui/components/SubmitPage/index.js
@@ -107,7 +107,7 @@ class SubmitPage extends Component {
         Meteor.call("games.hideWinner", this.props.game._id);
         Meteor.call("submissions.removeData", this.props.game._id);
       }
-    }, 7000);
+    }, 5000);
   }
 
   removeGame() {
